80c8a75097e472fe4f10b428f1a23eca72d32ca8
Maksim <m.bondarenko1988@yandex.ru>
2023-02-01 21:43:32 +0300

    Done third homework to five seminar about C sharp

---
+++ homework_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] RandomDoubleArray(int size)
+{
+    double[] arr = new double[size];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(rand.Next(0, 10) + rand.NextDouble(), 3);
+    }
+    return arr;
+}
+
+double DiffMaxMinElements(double [] array)
+{
+    double diff = 0;
+    Array.Sort(array);
+    if(array.Length>0)
+    {
+        diff = array[array.Length -1] - array[0];
+    }
+    return diff;
+}
+
+void PrintArray(double[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+double[] numbers = RandomDoubleArray(10);
+PrintArray(numbers);
+double result = DiffMaxMinElements(numbers);
+Console.WriteLine($"Разница между максимальным и минимальным елементом массива равна: {result}");
